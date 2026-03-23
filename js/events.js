1b19c27f15cddd1d6f3f452ac84cd37e8da61cd8
Eric Brousseau <emb417@gmail.com>
2026-03-22 22:42:04 -0700

    conditionally handling audio on table only, allowing bg and dmd to fade out

--- js/events.js
+++ js/events.js
@@ -4,47 +4,51 @@
  */
 
 async function receiveEvent(message) {
-    vpin.call("console_out", message); // Debug: send to Python CLI console
+  vpin.call("console_out", message); // Debug: send to Python CLI console
 
-    // Let VPinFECore handle the data refresh logic
-    await vpin.handleEvent(message);
+  // Let VPinFECore handle the data refresh logic
+  await vpin.handleEvent(message);
 
-    // Handle UI updates based on event type
-    switch (message.type) {
-        case "TableIndexUpdate":
-            currentTableIndex = message.index;
-            updateScreen();
-            break;
+  // Handle UI updates based on event type
+  switch (message.type) {
+    case "TableIndexUpdate":
+      currentTableIndex = message.index;
+      updateScreen();
+      break;
 
-        case "TableLaunching":
-            tableAudio.stop();
-            fadeOut();
-            break;
+    case "TableLaunching":
+      if (windowName === "table") {
+        tableAudio.stop();
+      }
+      fadeOut();
+      break;
 
-        case "TableLaunchComplete":
-            fadeIn();
-            if (windowName === "table") {
-                tableAudio.play(vpin.getAudioURL(currentTableIndex));
-            }
-            break;
+    case "TableLaunchComplete":
+      fadeIn();
+      if (windowName === "table") {
+        tableAudio.play(vpin.getAudioURL(currentTableIndex));
+      }
+      break;
 
-        case "RemoteLaunching":
-            tableAudio.stop();
-            showRemoteLaunchOverlay(message.table_name);
-            fadeOut();
-            break;
+    case "RemoteLaunching":
+      if (windowName === "table") {
+        tableAudio.stop();
+      }
+      showRemoteLaunchOverlay(message.table_name);
+      fadeOut();
+      break;
 
-        case "RemoteLaunchComplete":
-            hideRemoteLaunchOverlay();
-            fadeIn();
-            if (windowName === "table") {
-                tableAudio.play(vpin.getAudioURL(currentTableIndex));
-            }
-            break;
+    case "RemoteLaunchComplete":
+      hideRemoteLaunchOverlay();
+      fadeIn();
+      if (windowName === "table") {
+        tableAudio.play(vpin.getAudioURL(currentTableIndex));
+      }
+      break;
 
-        case "TableDataChange":
-            currentTableIndex = message.index;
-            updateScreen();
-            break;
-    }
+    case "TableDataChange":
+      currentTableIndex = message.index;
+      updateScreen();
+      break;
+  }
 }
